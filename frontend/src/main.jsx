17cc9c05c5a9a2e74b7ff24ff6ddc006ecb9a0d6
Nikhil Dadhich <nikhildadhich58@gmail.com>
2026-08-22 11:10:13 +0530

    Frontend updating main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import {
+  AuthProvider,
+} from "./context/AuthContext";
+
+import "./index.css";
+
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
   <React.StrictMode>
     <AuthProvider>
       <App />
